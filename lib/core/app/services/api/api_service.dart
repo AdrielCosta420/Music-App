02beed7882a9f8034b5a21596cee1195a78ace5e
Adriel Silva <adrielsilva_1990@hotmail.com>
2023-05-10 01:53:26 -0300

    criando os métodos da minha api service

--- lib/core/app/services/api/api_service.dart
+++ lib/core/app/services/api/api_service.dart
@@ -1,4 +1,56 @@
+import 'dart:developer';
+import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+import 'package:musicapp/core/errors/exceptions.dart';
+import 'package:musicapp/shared/models/genre_details_model.dart';
+import 'package:musicapp/shared/models/genre_model.dart';
 
-class ApiService {
-  
+class ApiService extends DioForNative {
+  ApiService([super.baseOptions]);
+
+  Future<List<GenreModel>> getGenres() async {
+    try {
+      const endPoint = '/genres';
+
+      final Response response = await get(endPoint);
+
+      return (response.data as List)
+          .map((genre) => GenreModel.fromMap(genre))
+          .toList();
+    } on DioError catch (dioError) {
+      log(
+        'Erro ao fazer o get dos genÊros musicais',
+        error: dioError,
+        stackTrace: dioError.stackTrace,
+      );
+      throw ApiExceptions(
+          message: 'Erro ao carregar genÊros',
+          statuscode: dioError.response?.statusCode.toString());
+    } catch (error) {
+      log('Erro ao fazer get dos gêneros musicais');
+      throw GeneralException();
+    }
+  }
+
+  Future<GenreDetailsModel> getGenreDetails({required String genre}) async {
+    try {
+      final endPoint = '/genres/$genre';
+
+      final Response response = await get(endPoint);
+
+      return GenreDetailsModel.fromMap(response.data);
+    } on DioError catch (dioError) {
+      log(
+        'Erro ao fazer o get dos detalhes do genêros musicais',
+        error: dioError,
+        stackTrace: dioError.stackTrace,
+      );
+      throw ApiExceptions(
+          message: 'Erro ao fazer o get dos detalhes do genêros musicais',
+          statuscode: dioError.response?.statusCode.toString());
+    } catch (error) {
+      log('Erro ao fazer get dos detalhes do gêneros musicais');
+    }
+    throw GeneralException();
+  }
 }
---
+++ lib/core/errors/exceptions.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ApiExceptions implements Exception {
+  final String message;
+  final String? statuscode;
+  ApiExceptions({
+    required this.message,
+    this.statuscode,
+  });
+}
+
+class GeneralException implements Exception {
+  final String message;
+  GeneralException({
+     this.message = 'Ocorreu um erro. Tente novamente.',
+  });
+}
